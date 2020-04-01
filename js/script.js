1d469e8e0fbe459781b821e19e8d240d8e6be559
Borbala Mester <mest0006@alognquinlive.com>
2020-04-01 09:57:47 -0400

    completes sassy shapes

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
 const colors = ['red', 'blue', 'green', 'purple', 'orange']
 const shapes = ['square', 'circle', 'pentagon', 'hexagon', 'octagon']
+
+let $main = document.querySelector('.shapes')
+let s = []
+for (let shape of shapes) {
+  for (let color of colors) {
+    s.push(`
+  <div class="${shape}-${color}">
+   </div> 
+`)
+
+
+
+  }
+}
+$main.innerHTML = s.join('')
